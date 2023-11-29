56e7e19a83867ae0cd51d462f5fa837e5b2ac888
Warwick Stone <warwick_stone@fastmail.com>
2023-11-29 14:44:58 +1100

    add domain test project

---
+++ src/orgcat.domain.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace orgcat.domain.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CreateSurvey()
+    {
+        Assert.Pass();
+    }
+}
