b51e85f0929527db1545dcecfd1d3fdd460704a6
Warwick Stone <warwick_stone@fastmail.com>
2023-12-02 22:09:07 +1100

    WIP: app runner cdk stack

--- infra/app_runner/orgat/lib/orgat-stack.ts
+++ infra/app_runner/orgat/lib/orgat-stack.ts
@@ -1,8 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_apprunner as apprunner } from 'aws-cdk-lib';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class OrgatStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const ecrRepo = new ecr.Repository(this, 'OrgCatRepo', {
+        repositoryName: 'orgcat-repo',
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const appRunner = new apprunner.CfnService(this, 'OrgCatRunner', {
+        serviceName: 'orgcat',
+        sourceConfiguration: {
+            autoDeploymentsEnabled: true,
+            authenticationConfiguration: {
+                accessRoleArn: 'arn:aws:iam::123456789012:role/aws-service-role/apprunner.amazonaws.com/AWSServiceRoleForAppRunner',
+            },
+            imageRepository: {
+                imageIdentifier: ecrRepo.repositoryUri,
+                imageRepositoryType: 'ECR',
+            },
+        },
+    });
   }
 }
