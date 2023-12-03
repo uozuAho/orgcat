5fc9363be2979f31b2ed3a06e02fa847add9a750
Warwick Stone <warwick_stone@fastmail.com>
2023-12-03 13:36:25 +1100

    set prod http port to 5001

--- src/orgcat.web/Program.cs
+++ src/orgcat.web/Program.cs
@@ -16,9 +16,8 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
+    app.Urls.Add("http://*:5001");
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseStaticFiles();
