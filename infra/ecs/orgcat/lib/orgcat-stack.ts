532fbb54daaed945e0c6982bb5ddb2575060b3c3
Warwick Stone <warwick_stone@fastmail.com>
2024-06-02 11:41:02 +1000

    WIP: ecs infra

--- infra/ecs/orgcat/lib/orgcat-stack.ts
+++ infra/ecs/orgcat/lib/orgcat-stack.ts
@@ -1,8 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import path = require('path');
+
+const projectRoot = path.resolve(__dirname, '../../../..');
+const srcDir = path.join(projectRoot, 'src');
 
 export class OrgcatStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+    const logging = new ecs.AwsLogDriver({
+        streamPrefix: "orgcat",
+        logRetention: 1,
+    });
+    const appImage = new DockerImageAsset(this, 'AppImage', {
+        directory: srcDir,
+    });
+    const taskDef = new ecs.FargateTaskDefinition(this, "TaskDef");
+    taskDef.addContainer("AppContainer", {
+      image: ecs.ContainerImage.fromDockerImageAsset(appImage),
+      memoryLimitMiB: 512,
+      logging,
+    });
+    new ecs.FargateService(this, "Service", {
+      cluster,
+      taskDefinition: taskDef,
+      assignPublicIp: true,
+    });
   }
 }
--- infra/ecs/readme.md
+++ infra/ecs/readme.md
@@ -2,5 +2,19 @@
 
 # Requirements
 - nodejs (tested with v20)
+- docker
 - aws cdk (npm i -g aws-cdk)
-- an AWS account
+- an AWS account with cli access
+
+# infra commands
+```sh
+cdk deploy
+cdk destroy
+```
+
+# todo
+- database:
+    - in ecs: orgcat.web.WebBuilder.ConfigureBuilder(WebApplicationBuilder builder) in /App/orgcat.web/WebBuilder.cs:line 15
+        - connection string is empty
+- db migrations
+- add output: service url
